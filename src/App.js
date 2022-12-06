cd067a31e94305d376754b7dfb862dfce60e6abd
MarsiaRochelin <marsrochelin@pursuit.org>
2022-12-06 15:34:50 -0500

    App.jss and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Home from "./components/Home"
+import Nav from './components /Nav';
+import About from './components /About';
+import Video from "./components/Video"
+
 import './App.css';
+
+//import BrowserRouter as {Router, Routes, Route} from 'react-router-dom'
+  //create each components and import them here
+  //to pass down router functionality wrap components inside of a router element inside the div element for the app
+  //define area that will display views by wrapping the components in the Routes element and by giving them individual Route(s) with the path and element attributes with-in the Routes 
+  //for each view import {Link} from "react router-dom"
+    //wrap each element in Link elements with to attributes in them to navigate user to a different views 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/video/:id" element={<Video />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
